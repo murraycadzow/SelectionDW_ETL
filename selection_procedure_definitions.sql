6ecc3b89facafab41e2f778b5902e631124459b4
nickb- <nick.dale.burns@gmail.com>
2016-07-08 14:13:04 +1200

    updated initial values for dimStat'

--- selection_procedure_definitions.sql
+++ selection_procedure_definitions.sql
@@ -45,10 +45,10 @@ begin
         FROM staging_gene as stage
         INNER JOIN dimPos as pos
           ON (
-	          pos.chrom = stage.chrom 
-	      AND pos.chrom_start = stage.start_position
-              AND pos.chrom_end = stage.end_position
-	);
+				pos.chrom = stage.chrom 
+			AND pos.chrom_start = stage.start_position
+            AND pos.chrom_end = stage.end_position
+		  );
 end //
 delimiter ;
 
@@ -67,7 +67,24 @@ begin
                ('Pi', NULL),
                ('FuLi_D', 'Fu and Lis D'),
                ('FuLi_F', 'Fu and Lis F'),
-               ('FayWu_H','Fay and Wus H');
+               ('FayWu_H','Fay and Wus H'),
+               ('ka', 'KAKS statistic'),
+               ('ks', 'KAKS statistic'),
+               ('kcomputed', 'KAKS ka / ks + 1'),
+               ('MAF', 'AF statistic'),
+               ('DAF', 'AF statistic'),
+               ('nsl_freq_1', NULL),
+               ('sL1', NULL),
+               ('sL0', NULL),
+               ('unstd_nsl', NULL),
+               ('norm_nsl', NULL),
+               ('significant_nsl', NULL),
+               ('ihs_freq_1', NULL),
+               ('ihh1', NULL),
+               ('ihh0', NULL),
+               ('unstd_ihs', NULL),
+               ('norm_ihs', NULL),
+               ('significant_ihs', NULL);
 end //
 delimiter ;
 
@@ -91,13 +108,12 @@ create procedure unstage (IN experiment_id int)
 begin
 	INSERT INTO intraSel (posID, popID, statValue, statID, experimentID)
 		SELECT d2.posID, d1.popID, st.statValue, d3.statID, experiment_id 
-                FROM staging_results as st
-                  INNER JOIN dimPopData as d1 on d1.pop = st.pop
-                  INNER JOIN dimPos as d2 on (
+        FROM staging_results as st
+          INNER JOIN dimPopData as d1 on d1.pop = st.pop
+          INNER JOIN dimPos as d2 on (
 				d2.chrom = st.chrom
-		            AND d2.chrom_start = st.chrom_start
-                            AND d2.chrom_end = st.chrom_end
-                  )
+			AND d2.chrom_start = st.chrom_start
+            AND d2.chrom_end = st.chrom_end)
 		  INNER JOIN dimStat as d3 on d3.statName = st.variable;
 end //
 delimiter ;
